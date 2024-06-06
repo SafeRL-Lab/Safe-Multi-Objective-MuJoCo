148ab1b128cdf66d189f971ec34a0c4e6d3d3c67
chauncygu <gshangd@163.com>
2024-06-05 17:54:37 -0700

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,1 +1,18 @@
-a
+import safe_mo_mujoco as gym
+
+import json
+import os
+import time
+
+
+env = gym.make("Ant-v4")
+# env = gym.make("Ant-v4", render_mode="human") # render environments
+
+observation, info = env.reset(seed=42)
+for i in range(1000):
+    action = env.action_space.sample()
+    observation, reward1, reward2, cost, terminated, truncated, info = env.step(action)
+    # env.render() # render environments
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
